6b8663f0e47f8ecb8232f7678ee27ee6215e09c9
Yassine Zemmouri <yzemmouri.eng@gmail.com>
2022-04-07 00:48:15 -0400

    formsubmit.co for contact form

--- js/google-maps.js
+++ js/google-maps.js
@@ -1,16 +1,44 @@
 
 function iniMap() {
-    const mapEl = document.getElementById('map');
-    const myLatLng = { lat: 42.28123722743706, lng: -71.118680753433};
-    const map = new google.maps.Map(mapEl, {
-      zoom: 13,
-      center: myLatLng,
-    });
+  // Using google api to retrieve lat long from address
+  // fetch('https://maps.googleapis.com/maps/api/geocode/json?address="564 Hyde Park Ave. Roslindale, MA"&key=my_key')
+  //   .then(res=>{
+  //     if(res.ok) return res.json();
+  //     else console.log('something went wrong');
+  //   }
+  //   )
+  // .then(data=>{
+  //   const myLatLng = (data.results[0].geometry.location);
+  //   console.log(myLatLng);
+
+ 
+  //   const mapEl = document.getElementById('map');
+  //   // const myLatLng = { lat: 42.28123722743706, lng: -71.118680753433};
+  //   console.log(myLatLng);
+  //   const map = new google.maps.Map(mapEl, {
+  //     zoom: 12,
+  //     center: myLatLng,
+  //     scrollwheel: false,
+  //   });
   
-    new google.maps.Marker({
-      position: myLatLng,
-      map,
-      title: "Connections, Inc./Home Care Specialists",
-    });
+  //   new google.maps.Marker({
+  //     position: myLatLng,
+  //     map,
+  //     title: "Connections, Inc./Home Care Specialists",
+  //   });
+  // })
+  const mapEl = document.getElementById('map');
+  const myLatLng = { lat: 42.28123722743706, lng: -71.118680753433};
+  const map = new google.maps.Map(mapEl, {
+    zoom: 12,
+    center: myLatLng,
+    scrollwheel: false,
+  });
+
+  new google.maps.Marker({
+    position: myLatLng,
+    map,
+    title: "Connections, Inc./Home Care Specialists",
+  });
 }
 google.maps.event.addDomListener(window, 'load', iniMap);
